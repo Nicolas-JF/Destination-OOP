fc6e84da0504c253ea599d643d0ef47a9efa2a4b
Nicolas Fabius <nicolas.fabius@nicolas.fabius-MacBook-Pro>
2024-11-05 14:46:33 -0500

    Updated Bag.js

--- classes/Bag.js
+++ classes/Bag.js
@@ -1,7 +1,17 @@
 class Bag {
+    constructor(weight, id) {
+        this.weight = weight;
+        this.id = id;
+        this._owner = null; // Private field
+    }
 
+    getOwner() {
+        return this._owner;
+    }
 
+    assignOwner(person) {
+        this._owner = person;
+    }
 }
-
 
 module.exports = Bag;
